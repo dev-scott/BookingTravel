1cc7d92f80ff99c74f645dfa6beb51c1f65c8929
Dev-scott <sadoscott25@gmail.com>
2023-02-01 22:40:49 +0100

    mise a jour

--- js/swiper.js
+++ js/swiper.js
@@ -1,4 +1,17 @@
-var mySwiper = new Swiper('.swiper-container', {
-    speed: 400,
-    spaceBetween: 100
-});
+
+var swiper = new Swiper(".slide-content" , {
+    slidesPerView:3,
+    spaceBetween:30,
+    slidesPerGroup:3,
+    loop:true,
+    loopFillGroupWithBlank:true,
+    pagination:{
+        el:"swiper-pagination",
+        clickable:true,
+
+    },
+    navigation:{
+        nextEl:"swiper-button-next",
+        prevEl:"swiper-button-prev",
+    }
+})
